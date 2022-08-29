555e3182642f7827bda732f6c66bbdb9a7d7ffcf
Filipe Furlan <73194798+f136pix@users.noreply.github.com>
2022-08-28 21:36:24 -0300

    Add files via upload

---
+++ src/Components/Container/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Titulo from "../Titulo";
+import Conta from "../Conta";
+import styled from "styled-components";
+import { Extrato } from "../Extrato";
+
+const StyledContainer = styled.div`
+  background-color: ${ ({theme}) => theme.body };
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color: ${({ theme }) => theme.text};
+  @media (max-width: 800px) {
+  flex-direction: column;
+  }
+`
+
+const Container = () => {
+  return (
+    <StyledContainer>
+      <Titulo>Olá Cliente!</Titulo>
+      <Conteudo>
+        <Conta />
+        <Extrato/>
+      </Conteudo>
+    </StyledContainer>
+  );
+};
+
+export default Container;
